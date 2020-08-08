92e8ac47bf709141bf96913a6e4239573a8cb5f7
kaveenhyacinth <alwiskaveen@gmail.com>
2020-08-08 08:20:09 +0530

    Updates

---
+++ precache-manifest.80f6af32e284b2fb06a727c1cd5ace58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6280cd58f5b70d587068fa2f4de63e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "21e4cc9cd0dbee5de0e3",
+    "url": "/tic-tac-toe/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "a2b1ef26c2b1fc5cd4e9",
+    "url": "/tic-tac-toe/static/css/main.0e5334f4.chunk.css"
+  },
+  {
+    "revision": "21e4cc9cd0dbee5de0e3",
+    "url": "/tic-tac-toe/static/js/2.32ac7d84.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.32ac7d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b1ef26c2b1fc5cd4e9",
+    "url": "/tic-tac-toe/static/js/main.b14237db.chunk.js"
+  },
+  {
+    "revision": "ed3277e286232d5390b2",
+    "url": "/tic-tac-toe/static/js/runtime-main.b831eec5.js"
+  }
+]);
